81aebc7171dd8e73804c9383ec5f8b7e3c30f3ff
LiLy Perera <lilycapetillo86@gmail.com>
2021-11-15 16:14:19 -0500

    Including api routers

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
-    path('new/', admin.site.urls),
+    path('', include(router.urls)),
     # path('api/', admin.site.urls),
 ]
